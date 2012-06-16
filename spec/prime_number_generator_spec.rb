04064a24459fd428b98baed621cd68d47d1bcfb7
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 16:57:30 +0300

    added the inverse range spec to the prime number generator spec

--- spec/prime_number_generator_spec.rb
+++ spec/prime_number_generator_spec.rb
@@ -18,6 +18,10 @@ describe PrimeNumberGenerator do
   	@prime_generator.generate(1, 13).should eq([2, 3, 5, 7, 11, 13])
   end
 
+  it "should handle reverse ranges (20,-2) == (-2,20)" do
+  	@prime_generator.generate(20, -2).should eq([2, 3, 5, 7, 11, 13, 17, 19])
+  end
+
   it "should return true for a prime number (17)" do
   	@prime_generator.is_prime?(17).should be_true
   end
