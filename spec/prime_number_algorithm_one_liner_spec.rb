e6b23470d2cfc67d3e8b80da7e59c31a658533c7
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 16:20:33 +0300

    adding a prime number algorithm spec

---
+++ spec/prime_number_algorithm_one_liner_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe PrimeNumberAlgorithmOneLiner do
+
+	it "should return true for prime numbers"
+
+  it "should return false for non-prime numbers"
+
+  it "should return true or flase depending if a number is prime or non-prime"
+
+end
