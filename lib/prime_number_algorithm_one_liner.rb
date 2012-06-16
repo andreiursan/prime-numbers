71929b4d32d7fa8684a9869468e7a0d3622632d6
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 16:38:32 +0300

    added behavior for the PrimeNumberAlgorithm one liner

--- lib/prime_number_algorithm_one_liner.rb
+++ lib/prime_number_algorithm_one_liner.rb
@@ -1,2 +1,5 @@
 class PrimeNumberAlgorithmOneLiner
+	def is_prime? number
+		number < 2 ? false : ('1' * number) !~ /^1?$|^(11+?)\1+$/
+	end
 end
--- spec/prime_number_algorithm_one_liner_spec.rb
+++ spec/prime_number_algorithm_one_liner_spec.rb
@@ -7,10 +7,18 @@ describe PrimeNumberAlgorithmOneLiner do
     @prime_number_algorithm_one_liner = PrimeNumberAlgorithmOneLiner.new
   end
 
-	it "should return true for prime numbers"
+	it "should return true for prime numbers" do
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 
+    83, 89, 97, 101, 7901, 7907, 7919].each do |number|
+      @prime_number_algorithm_one_liner.is_prime?(number).should be_true
+    end
+  end
 
-  it "should return false for non-prime numbers"
-
-  it "should return true or flase depending if a number is prime or non-prime"
+  it "should return false for non-prime numbers" do
+    [832, 1, -23, 0, 63, 888812, 15].each do |number|
+      @prime_number_algorithm_one_liner.is_prime?(number).should be_false
+    end
+  end
 
 end
