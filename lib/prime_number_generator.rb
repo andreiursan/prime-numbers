1108e58306184e83aac9f109ea7e2d7da96394d6
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 15:46:15 +0300

    added is_prime? method to PrimeNumberGenerator

--- lib/prime_number_generator.rb
+++ lib/prime_number_generator.rb
@@ -1,5 +1,9 @@
 class PrimeNumberGenerator
-	def generate
-		
-	end
+  def generate
+
+  end
+
+  def is_prime?
+    		
+  end
 end
--- spec/prime_number_generator_spec.rb
+++ spec/prime_number_generator_spec.rb
@@ -2,20 +2,22 @@ require 'spec_helper'
 
 describe PrimeNumberGenerator do
 
-	before(:all) do
-		@prime_generator = PrimeNumberGenerator.new
-	end
+  before(:all) do
+	@prime_generator = PrimeNumberGenerator.new
+  end
 
-	it "should respond to message generate" do
-		@prime_generator.should respond_to(:generate)
-	end
+  it "should respond to message generate" do
+	@prime_generator.should respond_to(:generate)
+  end
 
-	it "should respond to message is_prime?"
+  it "should respond to message is_prime?" do
+	@prime_generator.should respond_to(:is_prime?)
+  end
 
-	it "should return all prime numbers between 1 and 13"
+  it "should return all prime numbers between 1 and 13"
 
-	it "should return true for a prime number (17)"
+  it "should return true for a prime number (17)"
 
-	it "should return false if a number isn't prime (18)"
+  it "should return false if a number isn't prime (18)"
 
 end
