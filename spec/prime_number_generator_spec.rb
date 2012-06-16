59457168c41a872fa7c73f0036dc564ccd6fa5fa
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 17:10:44 +0300

    some indentation fixes, forgot to configure sublime on this mac

--- spec/prime_number_generator_spec.rb
+++ spec/prime_number_generator_spec.rb
@@ -3,31 +3,31 @@ require 'spec_helper'
 describe PrimeNumberGenerator do
 
   before(:all) do
-	@prime_generator = PrimeNumberGenerator.new(PrimeNumberAlgorithmOneLiner.new)
+    @prime_generator = PrimeNumberGenerator.new(PrimeNumberAlgorithmOneLiner.new)
   end
 
   it "should respond to message generate" do
-	@prime_generator.should respond_to(:generate)
+    @prime_generator.should respond_to(:generate)
   end
 
   it "should respond to message is_prime?" do
-	@prime_generator.should respond_to(:is_prime?)
+    @prime_generator.should respond_to(:is_prime?)
   end
 
   it "should return all prime numbers between 1 and 13" do
-  	@prime_generator.generate(1, 13).should eq([2, 3, 5, 7, 11, 13])
+    @prime_generator.generate(1, 13).should eq([2, 3, 5, 7, 11, 13])
   end
 
   it "should handle reverse ranges (20,-2) == (-2,20)" do
-  	@prime_generator.generate(20, -2).should eq([2, 3, 5, 7, 11, 13, 17, 19])
+    @prime_generator.generate(20, -2).should eq([2, 3, 5, 7, 11, 13, 17, 19])
   end
 
   it "should return true for a prime number (17)" do
-  	@prime_generator.is_prime?(17).should be_true
+    @prime_generator.is_prime?(17).should be_true
   end
 
   it "should return false if a number isn't prime (18)" do
-  	@prime_generator.is_prime?(18).should be_false
+    @prime_generator.is_prime?(18).should be_false
   end
 
 end
