7771af0f71cb6bba1f508d8aa9a8d45bb2354461
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-16 15:31:39 +0300

    writing specs, Yellow mode, to design the PrimeNumberGenerator API

--- spec/prime_number_generator_spec.rb
+++ spec/prime_number_generator_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe PrimeNumberGenerator do
+	it "should respond to message generate" 
+
+	it "should respond to message is_prime?"
+
+	it "should return all prime numbers between 1 and 13"
+
+	it "should return true for a prime number (17)"
+
+	it "should return false if a number isn't prime (18)"
 	
 end
