b4b4d6477962de94ccac7e192b6566a16b67d926
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-17 17:30:19 +0300

    added the spec for our new algorithm Sieve of Eratosthenes, which also generates the prime numbers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
 require_relative "../lib/prime-numbers.rb"
 require_relative "../lib/prime-numbers/algorithm_one_liner.rb"
+require_relative "../lib/prime-numbers/algorithm_sieve_of_eratosthenes.rb"
