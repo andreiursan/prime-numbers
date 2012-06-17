ab2be8641b3a7694663f08304471beae9169a5fe
Andrei Ursan <ursan.andrei@gmail.com>
2012-06-17 18:20:10 +0300

    added the generate method to one_liner algorithm

--- lib/prime-numbers/algorithms/one_liner.rb
+++ lib/prime-numbers/algorithms/one_liner.rb
@@ -1,5 +1,13 @@
 module PrimeNumbers
   class AlgorithmOneLiner
+    def generate a, b
+      primenumbers = []
+      (a..b).each do |number|
+        primenumbers << number if self.is_prime? number
+      end
+      primenumbers
+    end
+
     def is_prime? number
       number < 2 ? false : ('1' * number) !~ /^1?$|^(11+?)\1+$/
     end
